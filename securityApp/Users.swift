3da412337fc5b99e675fcff4b6001a666eb5e467
Jimenez Melendez Miguel Angel <miguejm@outlook.es>
2020-05-20 22:36:43 -0700

    Registration with web service; error returning username

---
+++ securityApp/Users.swift
@@ -0,0 +1,9 @@
+//
+//  Users.swift
+//  securityApp
+//
+//  Created by Miguel Angel Jimenez Melendez on 20/05/20.
+//  Copyright © 2020 Miguel Angel Jimenez Melendez. All rights reserved.
+//
+
+import Foundation
---
+++ securityApp/ViewControllerRegister.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerRegister.swift
+//  securityApp
+//
+//  Created by Miguel Angel Jimenez Melendez on 20/05/20.
+//  Copyright © 2020 Miguel Angel Jimenez Melendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerRegister: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
