0620c55f99c4cf3ce4d9f47b2beb0bc5cefe26f3
Jimenez Melendez Miguel Angel <miguejm@outlook.es>
2020-05-26 17:07:51 -0700

    Clase donde se implementa el codigo para mostrar el contenido de las ubicaciones, asi mostrar en el mapa de la aplicación

--- securityApp/ViewControllerMap.swift
+++ securityApp/ViewControllerMap.swift
@@ -9,26 +9,65 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
-class ViewControllerMap: UIViewController {
-
+class ViewControllerMap: UIViewController, CLLocationManagerDelegate{
+    
+    var location: String = ""
+    var latitude: String = ""
+    var longitude: String = ""
+    
+    var Reportes = [Reporte]()
+    
+    let dataJsonUrlClass = JsonClass()
+    let locationManager: CLLocationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
-        self.view.addSubview(mapView)
-
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-        // Do any additional setup after loading the view.
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        Reportes.removeAll()
+        
+        let data_to_send = ["id": ""] as NSMutableDictionary
+        dataJsonUrlClass.arrayFromJson(url: "webservice/getreports.php", data_send: data_to_send) {
+          (array_request) in
+            DispatchQueue.main.async {
+                let cuenta = array_request?.count
+                for indice in stride(from: 0, to: cuenta!, by: 1){
+                    let report = array_request?.object(at: indice) as! NSDictionary
+                    let location = report.object(forKey: "location") as! String?
+                    let latitude = report.object(forKey: "latitude") as! String?
+                    let longitude = report.object(forKey: "longitud") as! String?
+                    let description = report.object(forKey: "description") as! String?
+                    let score = report.object(forKey: "score") as! String?
+                    self.Reportes.append(Reporte(location: location, latitude: latitude, longitude: longitude, description: description, score: score))
+                }
+            }
+        }
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for currentLocation in locations{
+            location = "\(currentLocation.coordinate.latitude), \(currentLocation.coordinate.longitude)"
+            latitude = "\(currentLocation.coordinate.longitude)"
+            longitude = "\(currentLocation.coordinate.longitude)"
+        }
+        let camera = GMSCameraPosition.camera(withLatitude: Double(Double(latitude)!), longitude: Double(Double(longitude)!), zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
+        
+        for report in Reportes{
+            let marker = GMSMarker()
+            var latitud = report.latitude!
+            var longitud = report.longitude!
+            var location = report.location!
+            var score = report.score!
+            
+            marker.position = CLLocationCoordinate2D(latitude: Double(Double(latitud)!), longitude: Double(Double(longitud)!))
+            marker.title = location
+            marker.snippet = score
+            marker.map = mapView
+            print(marker.position)
+        }
+    }
+    
 }
